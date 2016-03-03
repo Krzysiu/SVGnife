3d2cd6d68460afa16a76036e2102624afb832554
Krzysiu <github@krzysiu.net>
2016-03-03 23:38:48 +0100

    allow pango tags in description

--- dialogs/about.php
+++ dialogs/about.php
@@ -3,13 +3,15 @@
 	
 	function showAboutDialog() {
 		global $dialogAbout, $appVer;
-		
 		$dialogAbout = new GladeXML('resources/dialogAbout.glade');
 		$_dialogAbout = $dialogAbout->get_widget('_dialogAbout');
-		$_dialogAbout->connect_simple('response', 'closeAboutDialog');
 		$_dialogAbout->connect('activate-link', 'openURLHook');
+		$_dialogAbout->connect_simple('response', 'closeAboutDialog');
+		$_dialogAbout->set_version($appVer);		
 		
-		$_dialogAbout->set_version($appVer);
+		$_aboutDescription = $_dialogAbout->get_children()[0]->get_children()[0]->get_children()[2];
+		$_aboutDescription->set_use_markup(true);
+		$_aboutDescription->connect('activate-link', 'openURLHook');
 	}		
 	
 	function closeAboutDialog() {
